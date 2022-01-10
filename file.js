3d2b0e898125d6231a35e3e74ce5bc118854d0ab
Lakshmidharan <91528149+Lakshmidharan@users.noreply.github.com>
2022-01-09 20:33:38 -0800

    Update file.js

--- file.js
+++ file.js
@@ -3,11 +3,15 @@ const param=process.argv[2]
 fs.readdir(param,{ withFileTypes: true }, function (err, files) 
 {
     if (err) throw err;
-    else 
-    {
-        files.forEach(filetype => 
+   
+        files.forEach((filetype)=> 
         {
-            console.log(filetype);
+            if(filetype.isFile){
+                console.log(`${filetype.name}:file`);
+            }
+            else{
+                console.log(`${filetype.name}:folder`);
+            }
         });
-    };
+   
 })
